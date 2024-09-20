7b1ccf9f76dcfb170acaea696748065d4a3d7372
Bert de Bruijn <4983172-bert.debruijn@users.noreply.gitlab.com>
2024-09-20 18:57:56 -0400

    Complete first quiz

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -10,7 +10,13 @@
 
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
-// fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(nr_apples: i32) -> i32 {
+    if nr_apples <= 40 {
+        2 * nr_apples
+    } else {
+        nr_apples
+    }
+}
 
 fn main() {
     // You can optionally experiment here.
