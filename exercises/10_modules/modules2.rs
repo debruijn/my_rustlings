10f40550cd3f14e6c977c0537fa46624a7854811
Bert de Bruijn <4983172-bert.debruijn@users.noreply.gitlab.com>
2024-09-21 19:12:19 -0400

    Complete chapters 8, 9 and 10

--- exercises/10_modules/modules2.rs
+++ exercises/10_modules/modules2.rs
@@ -2,17 +2,16 @@
 // the `use` and `as` keywords.
 
 #[allow(dead_code)]
-mod delicious_snacks {
-    // TODO: Add the following two `use` statements after fixing them.
-    // use self::fruits::PEAR as ???;
-    // use self::veggies::CUCUMBER as ???;
+pub mod delicious_snacks {
+    pub use self::fruits as fruit;
+    pub use self::veggies as veggie;
 
-    mod fruits {
+    pub mod fruits {
         pub const PEAR: &str = "Pear";
         pub const APPLE: &str = "Apple";
     }
 
-    mod veggies {
+    pub mod veggies {
         pub const CUCUMBER: &str = "Cucumber";
         pub const CARROT: &str = "Carrot";
     }
@@ -21,7 +20,7 @@ mod delicious_snacks {
 fn main() {
     println!(
         "favorite snacks: {} and {}",
-        delicious_snacks::fruit,
-        delicious_snacks::veggie,
+        delicious_snacks::fruit::PEAR,
+        delicious_snacks::veggie::CARROT,
     );
 }
